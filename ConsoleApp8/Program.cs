fbc20d63aae6308067aac4949587de95423fcea9
kostosspb <39032757+kostosspb@users.noreply.github.com>
2020-09-15 15:59:31 +0300

    Added new line

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApp8
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("GitHub Test 15.09.2020");
         }
     }
 }
